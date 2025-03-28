06fe47cc4ab4d184d082a802c89afe22215d713c
Ilmi28 <ilmialiev28@gmail.com>
2025-03-29 00:42:42 +0100

    refactoring #5

--- src/ShopListApp.Core/Commands/Auth/RegisterUserCommand.cs
+++ src/ShopListApp.Core/Commands/Auth/RegisterUserCommand.cs
@@ -1,6 +1,18 @@
-﻿namespace ShopListApp.Core.Commands.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopListApp.Commands
 {
-    public class RegisterUserCommand
+    public class CreateUserCommand
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public required string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")]
+        public required string Password { get; set; }
     }
 }
--- src/ShopListApp.Core/Interfaces/IServices/IUserService.cs
+++ src/ShopListApp.Core/Interfaces/IServices/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using ShopListApp.Commands;
+using ShopListApp.Core.Commands.Auth;
 using ShopListApp.Core.Commands.Delete;
 using ShopListApp.ViewModels;
 
@@ -6,7 +7,7 @@ namespace ShopListApp.Interfaces
 {
     public interface IUserService
     {
-        Task CreateUser(CreateUserCommand cmd);
+        Task CreateUser(RegisterUserCommand cmd);
         Task DeleteUser(string id, DeleteUserCommand cmd);
         Task UpdateUser(string id, UpdateUserCommand updatedUser);
         Task<UserResponse> GetUserById(string id);
